6868e856ac02370ac4e8249cc989b1f646396228
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-18 14:16:36 -0700

    Modifications

--- src/Badges/Badges.tsx
+++ src/Badges/Badges.tsx
@@ -6,7 +6,6 @@ function Badges() {
     return (
         <div className="badges">
             <img src={`https://img.shields.io/netlify/${process.env.REACT_APP_SITE_ID}?style=for-the-badge`} alt={""} /><br />
-            <img src="https://img.shields.io/website?style=for-the-badge&url=https%3A%2F%2Fmeme-creator-app.netlify.app" alt={""} />
         </div>
     )
 }
