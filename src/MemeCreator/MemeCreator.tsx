2bbfec0e448dd0e9ec467a47952ab9065354fb95
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-18 10:43:30 -0700

    Added Watermark Checkbox

--- src/MemeCreator/MemeCreator.tsx
+++ src/MemeCreator/MemeCreator.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Input, Button, InputLabel } from '@material-ui/core';
+import { Input, Button, InputLabel, Checkbox, FormControlLabel } from '@material-ui/core';
 
 function MemeCreator() {
 
@@ -11,6 +11,8 @@ function MemeCreator() {
 
     const [width, setWidth] = useState(640);
     const [height, setHeight] = useState(320);
+
+    const [watermark, setWatermark] = useState(true);
 
     const filterText = (text: string) => {
         const newString = text;
@@ -29,7 +31,7 @@ function MemeCreator() {
         if (topText === "" || bottomText === "" || imageLink === "") return;
         const t = filterText(topText);
         const b = filterText(bottomText);
-        setMemeLink(`https://memegen.link/custom/${t}/${b}.jpg?width=${width}&height=${height}&alt=${imageLink}`);
+        setMemeLink(`https://memegen.link/custom/${t}/${b}.jpg?width=${width}&height=${height}&alt=${imageLink}&watermark=${watermark}`);
     }
 
     const copyLink = () => navigator.clipboard.writeText(memeLink);
@@ -41,6 +43,7 @@ function MemeCreator() {
             <InputLabel>Bottom Text: </InputLabel><Input value={bottomText} onChange={(e) => setBottomText(e.target.value)} /><br />
             <InputLabel>Image URL: </InputLabel><Input type="url" value={imageLink} onChange={(e) => setImageLink(e.target.value)} /><br />
             <Input value={width} onChange={(e) => setWidth(parseInt(e.target.value))} className="width size" /> x <Input value={height} onChange={(e) => setHeight(parseInt(e.target.value))} className="height size" /><br /><br />
+            <FormControlLabel control={<Checkbox name="Watermark" onChange={(e) => setWatermark(e.target.checked)} checked={watermark} />} label="Watermark" /><br />
             <Button variant="contained" color="primary" onClick={createMeme}>Create Meme!</Button>
         </div>
     )
