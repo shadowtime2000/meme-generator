f616d182e5eda580cda81fcf8c4888a81755967f
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-19 10:51:07 -0700

    Fixed Bug With Converting Characters To Be Used By memegen.link

--- src/MemeCreator/MemeCreator.tsx
+++ src/MemeCreator/MemeCreator.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Input, Button, InputLabel, Checkbox, FormControlLabel } from '@material-ui/core';
+import { Input, Button, InputLabel, Checkbox, FormControlLabel, MenuItem, Select } from '@material-ui/core';
 
 import Meme from '../Meme/Meme';
 
@@ -17,15 +17,15 @@ function MemeCreator() {
     const [watermark, setWatermark] = useState(true);
 
     const filterText = (text: string) => {
-        const newString = text;
-        newString.replace(/ /, "_");
-        newString.replace(/_/, "__");
-        newString.replace(/-/, "--");
-        newString.replace(/\\?/, "~q");
-        newString.replace(/%/, "~p");
-        newString.replace(/#/, "~h");
-        newString.replace(/\//, "~s");
-        newString.replace(/"/, "''");
+        let newString = text;
+        newString = newString.replace("_", "__");
+        newString = newString.replace(" ", "_");
+        newString = newString.replace("-", "--");
+        newString = newString.replace("?", "~q");
+        newString = newString.replace(/%/, "~p");
+        newString = newString.replace(/#/, "~h");
+        newString = newString.replace(/\//, "~s");
+        newString = newString.replace(/"/, "''");
         return newString;
     }
 
@@ -33,7 +33,7 @@ function MemeCreator() {
         if (topText === "" || bottomText === "" || imageLink === "") return;
         const t = filterText(topText);
         const b = filterText(bottomText);
-        setMemeLink(`https://memegen.link/custom/${t}/${b}.jpg?alt=${imageLink}&width=${width}&height=${height}&watermark=${watermark}`);
+        setMemeLink(`https://memegen.link/custom/${t}/${b}.jpg?alt=${encodeURIComponent(imageLink)}&width=${width}&height=${height}&watermark=${watermark}`);
     }
 
     return (
@@ -41,7 +41,20 @@ function MemeCreator() {
             <Meme memeLink={memeLink} /><br />
             <InputLabel>Top Text: </InputLabel><Input value={topText} onChange={(e) => setTopText(e.target.value)} /><br />
             <InputLabel>Bottom Text: </InputLabel><Input value={bottomText} onChange={(e) => setBottomText(e.target.value)} /><br />
-            <InputLabel>Image URL: </InputLabel><Input type="url" value={imageLink} onChange={(e) => setImageLink(e.target.value)} /><br />
+            <InputLabel>Image URL: </InputLabel><Input type="url" value={imageLink} onChange={(e) => setImageLink(e.target.value)} /> or 
+            <InputLabel>Select Meme</InputLabel>
+            <Select onChange={(e) => setImageLink(e.target.value as string)}>
+                <MenuItem value={"https://memegen.link/buzz/"}>Buzz Lightyear</MenuItem>
+                <MenuItem value={"https://memegen.link/afraid/"}>Afraid To Ask Andy</MenuItem>
+                <MenuItem value={"https://memegen.link/trump/"}>Donald Trump</MenuItem>
+                <MenuItem value={"https://memegen.link/spongebob/"}>Spongebob Squarepants</MenuItem>
+                <MenuItem value={"https://memegen.link/tenguy/"}>Ten Guy</MenuItem>
+                <MenuItem value={"https://memegen.link/sad-biden/"}>Sad Joe Biden</MenuItem>
+                <MenuItem value={"https://memegen.link/sad-boehner/"}>Sad John Boehner</MenuItem>
+                <MenuItem value={"https://memegen.link/sad-clinton/"}>Sad Bill Clinton</MenuItem>
+                <MenuItem value={"https://memegen.link/sad-obama/"}>Sad Barack Obama</MenuItem>
+                <MenuItem value={"https://memegen.link/patrick/"}>Patrick Star</MenuItem>
+            </Select><br />
             <Input type="number" value={width} onChange={(e) => setWidth(parseInt(e.target.value))}/> x <Input value={height} onChange={(e) => setHeight(parseInt(e.target.value))} type="number" /><br /><br />
             <FormControlLabel control={<Checkbox name="Watermark" onChange={(e) => setWatermark(e.target.checked)} checked={watermark} />} label="Watermark" /><br />
             <Button variant="contained" color="primary" onClick={createMeme}>Create Meme!</Button>
