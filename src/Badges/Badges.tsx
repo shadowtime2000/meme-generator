b96772690adf658b2ca56e38a5cfc47fa3871777
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-18 17:50:07 -0700

    Another Badge

--- src/Badges/Badges.tsx
+++ src/Badges/Badges.tsx
@@ -6,6 +6,7 @@ function Badges() {
     return (
         <div className="badges">
             <img src={`https://img.shields.io/netlify/${process.env.REACT_APP_SITE_ID}?style=for-the-badge`} alt={""} /><br />
+            <img src="https://img.shields.io/github/languages/code-size/shadowtime2000/meme-generator?style=for-the-badge" alt={""} /><br />
             <a href="https://github.com/shadowtime2000/meme-generator/fork"><img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=for-the-badge" alt={""} /></a><br/>
         </div>
     )
