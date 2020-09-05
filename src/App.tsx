4715f900dedf7ac13527548e897ec45f9765da73
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-04 22:28:03 -0700

    Lazy Loading Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
-
-import Header from './NoReRender/Header';
-import Footer from './NoReRender/Footer';
-import MemeCreator from './MemeCreator/MemeCreator';
-import Badges from './Badges/Badges';
-
+import React, { lazy, Suspense } from 'react';
 import { Grid } from '@material-ui/core';
 
 import './App.css';
 
+const Header = lazy(() => import("./NoReRender/Header"));
+const Footer = lazy(() => import("./NoReRender/Footer"));
+const MemeCreator = lazy(() => import("./MemeCreator/MemeCreator"));
+const Badges = lazy(() => import("./Badges/Badges"));
+
+
 function App() {
   return (
     <div className="App">
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Header head="Meme Generator" />
-          <Badges />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Header head="Meme Generator" />
+            <Badges />
+          </Grid>
+          <Grid item xs={12}>
+            <MemeCreator /><br />
+          </Grid>
+          <Grid item xs={12}>
+            <Footer link="https://memegen.link" cleanLink="memegen.link" />
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <MemeCreator /><br />
-        </Grid>
-        <Grid item xs={12}>
-          <Footer link="https://memegen.link" cleanLink="memegen.link" />
-        </Grid>
-      </Grid>
+      </Suspense>
     </div>
   );
 }
