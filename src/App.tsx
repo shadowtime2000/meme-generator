f497ee30c995c97dd42b97905eb12396f1910866
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-04 22:38:02 -0700

    Using @loadable/component for loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
+import loadable from '@loadable/component';
 import { Grid } from '@material-ui/core';
 
 import './App.css';
 
-const Header = lazy(() => import("./NoReRender/Header"));
-const Footer = lazy(() => import("./NoReRender/Footer"));
-const MemeCreator = lazy(() => import("./MemeCreator/MemeCreator"));
-const Badges = lazy(() => import("./Badges/Badges"));
+const Header = loadable(() => import("./NoReRender/Header"));
+const Footer = loadable(() => import("./NoReRender/Footer"));
+const MemeCreator = loadable(() => import("./MemeCreator/MemeCreator"));
+const Badges = loadable(() => import("./Badges/Badges"));
 
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Header head="Meme Generator" />
-            <Badges />
-          </Grid>
-          <Grid item xs={12}>
-            <MemeCreator /><br />
-          </Grid>
-          <Grid item xs={12}>
-            <Footer link="https://memegen.link" cleanLink="memegen.link" />
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Header head="Meme Generator" />
+          <Badges />
         </Grid>
-      </Suspense>
+        <Grid item xs={12}>
+          <MemeCreator /><br />
+        </Grid>
+        <Grid item xs={12}>
+           <Footer link="https://memegen.link" cleanLink="memegen.link" />
+        </Grid>
+      </Grid>
     </div>
   );
 }
