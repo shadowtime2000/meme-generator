5bd3f47bc46065b987ce046c90c980387b9c279e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-06 11:40:36 -0700

    Changes

--- src/Meme/Meme.tsx
+++ src/Meme/Meme.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import loadable from '@loadable/component';
 
-const ShareButton = loadable(() => import( /* webpackChunkName: "sharebutton" */ "../ShareButton/ShareButton"));
+const ShareButton = loadable(() => import( /* webpackChunkName: "share" */ "../ShareButton/ShareButton"));
 
 type Props = {
     memeLink: string
